d100352e2ca180a7bd67d987fb8acdbb85860333
Emma <emmauw@email.com>
2026-01-12 20:40:29 +0300

    remove fetch for now

--- src/components/GitHubRepoItem.tsx
+++ src/components/GitHubRepoItem.tsx
@@ -1,31 +1,11 @@
-import React from 'react';
-import useSWR from 'swr';
-
-const fetcher = (url: string) => {
-  const headers: HeadersInit = {};
-  if (process.env.REACT_APP_GITHUB_TOKEN) {
-    headers.Authorization = `token ${process.env.REACT_APP_GITHUB_TOKEN}`;
-  }
-  return fetch(url, { headers }).then((res) => res.json());
-};
-
 interface Props {
   owner: string;
   repo: string;
 }
 
 export default function GitHubRepoItem({ owner, repo }: Props) {
-  const { data: repoData } = useSWR(`https://api.github.com/repos/${owner}/${repo}`, fetcher);
-  const { data: release } = useSWR(`https://api.github.com/repos/${owner}/${repo}/releases/latest`, fetcher);
-
-  if (!repoData) {
-    return (
-      <a href={`https://github.com/${owner}/${repo}`} className="github-repo md-source" target="_blank" rel="noopener noreferrer" title="GitHub repository">
-        <div className="md-source__icon md-icon"></div>
-        <div className="md-source__repository">{owner}/{repo}</div>
-      </a>
-    );
-  }
+  const repoData = { stargazers_count: 126, forks_count: 32 };
+  const release = { tag_name: 'v2.5.8' };
 
   return (
     <a href={`https://github.com/${owner}/${repo}`} className="github-repo md-source" target="_blank" rel="noopener noreferrer" title="GitHub repository">
